d49ec6a2a799ffc348b03b9d7a67a269ca071850
Erica Hicks <48265629+ericahicks@users.noreply.github.com>
2022-08-05 01:00:35 -0400

    working on submitting reservation updates and switching components on submit bug

--- src/app/reservation-update/reservation-update.component.ts
+++ src/app/reservation-update/reservation-update.component.ts
@@ -9,16 +9,25 @@ import { ReservationService } from '../reservation.service';
 })
 export class ReservationUpdateComponent implements OnInit {
 
-  @Input() theReservation?: ResrvationInfoDto;
+  @Input() theReservation?: ResrvationInfoDto; // is the selected reservation to update
   @Output() changeTheReservation = new EventEmitter<ResrvationInfoDto>();
+
+  @Input() selectedReservation?: ResrvationInfoDto; // maps to parent's selected reservation
+  @Output() changeSelectedReservation = new EventEmitter<ResrvationInfoDto>();
+
+  returnToView(): void {
+    this.selectedReservation = undefined;
+    this.changeSelectedReservation.emit(undefined); // deselect and return to VIEW list of existing revs page
+  }
+
   reset(): void {
     this.theReservation = undefined;
-    this.changeTheReservation.emit(this.theReservation);
+    this.changeTheReservation.emit(undefined); // deselect and return to SEARCH for existing revs page
   }
 
   update(reservation: ResrvationInfoDto) {
     console.log("updating the reservation - hi I'm the reservation update component");
-    this.reservationService.updateReservation(reservation).subscribe(data => this.theReservation = data);
+    this.reservationService.updateReservation(reservation).subscribe(data => {this.theReservation = data; console.log(data)});
     this.changeTheReservation.emit(this.theReservation);
   }
 
