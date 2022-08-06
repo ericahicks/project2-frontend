3327b5dd06567644e1fe5a91a513ba478910e462
Starprizzy <amiralaom@gmail.com>
2022-08-06 12:02:28 -0400

    changed number --> phonenumber in user components

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -2,15 +2,15 @@ export class User {
     id: number;
     firstName: string;
     lastName: string;
-    number: string;
+    phonenumber: string;
     email: string;
 
     constructor(id: number = 0, firstName: string = "No", lastName: string = "Body", 
-    number: string = "(000) 555-1234", email: string = "no@body.com") {
+    phonenumber: string = "(000) 555-1234", email: string = "no@body.com") {
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
-        this.number = number;
+        this.phonenumber = phonenumber;
     }
 }
